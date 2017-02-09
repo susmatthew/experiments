250ae4ba2d59aab9353c353951a8396b19212a63
susmatthew <matthew@suspectdeviceaudio.com>
2017-02-09 09:32:06 -0500

    moved things around

--- demo.c
+++ demo.c
@@ -4,10 +4,13 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <time.h>
 
 int main (int argc, char *argv[]) {
 	
 	uint8_t maxx = 0,maxy = 0,x=1,y=1;
+	uint16_t time = 0;
+	clock_t curr_time;
 
 	initscr();
 	noecho();
@@ -15,11 +18,14 @@ int main (int argc, char *argv[]) {
 	clear();
 	
 	while (1) {
-	
-	getmaxyx (stdscr,maxy,maxx);
-	
-	mvprintw(x,y,"x:%i, y:%i",maxx,maxy);
-	refresh();
+
+		getmaxyx (stdscr,maxy,maxx);
+		curr_time=clock();
+		time = curr_time/(CLOCKS_PER_SEC/100);
+		if ((time%1000)==0) clear();
+		mvprintw(((time/100)%32),x,"x:%i, y:%i",maxx,maxy);
+		mvprintw(y+1,x,"time:%u",time);
+		refresh();
 	}
 	
 	endwin();
